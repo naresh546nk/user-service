a1ca4957285872b9abfc4125e85d62897ed1f43d
naresh546nk <naresh546.nk@gmail.com>
2022-01-08 10:14:06 +0530

    added loadbalancer

--- src/main/java/com/user/repo/DepartmentProxy.java
+++ src/main/java/com/user/repo/DepartmentProxy.java
@@ -1,5 +1,6 @@
 package com.user.repo;
 
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +9,7 @@ import com.user.vo.Department;
 
 //@FeignClient(name="DEPARTMENT-SERVICE")
 @FeignClient(name="https://depart-service.herokuapp.com")
+@LoadBalancerClient
 public interface DepartmentProxy {
 	
 	@GetMapping("/departments/{id}")
